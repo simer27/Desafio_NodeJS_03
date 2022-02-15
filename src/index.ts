58d0f7f887c1bef9ae7e28448f8d772c9300b551
Simeao Almeida <simer27@yahoo.com>
2022-02-15 13:55:38 -0300

    3º Desafio Concluído

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { readvSync } from "fs";
 
 dotenv.config();
 
@@ -14,7 +15,7 @@ dotenv.config();
  */
 
 if (!process.env.PORT) {
-	process.exit(1);
+  process.exit(1);
 }
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
@@ -34,10 +35,34 @@ app.use(express.json());
  */
 
 app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 
+  // CÓDIGO PARA ATENDER OS REQUERIMENTOS
+  // R01, R02, R03, R04, R05
 
-	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
-	// R01, R02, R03, R04, R05
-	
+  // R01
+  const entrada = require("prompt-sync")({ sigint: true });
+  //const Aluno = new Alunos();
+  let q;
+  var NotaMaior: number = 0;
+  var NomeAluno: string = "";
+  var name: string;
+  var nota: number;
+
+  console.log("");
+  console.log("DESAFIO 03.");
+  console.log("");
+
+  q = parseInt(entrada("Qual a quantidade de alunos nesta sala? "));
+  for (let i = 1; i <= q; i++) {
+    name = entrada(`Nome do ${i}º aluno: `);
+    nota = parseFloat(entrada(`Nota do aluno ${name}: `));
+    if (nota > NotaMaior) {
+      NotaMaior = nota;
+      NomeAluno = name;
+    }
+  }
+  console.log("");
+  console.log(`Alunos com a maior Nota da Classe:`);
+  console.log(`${NomeAluno}, com a Nota ${NotaMaior}`);
 });
